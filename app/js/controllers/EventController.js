f327c120d961fd7c3d47e6891e39aa41c7d64ed5
Darshan Jain <jaindarshan27.97@gmail.com>
2016-12-06 02:07:21 +0530

    voting buttons added

--- app/js/controllers/EventController.js
+++ app/js/controllers/EventController.js
@@ -6,25 +6,46 @@ function  EventController($scope){
 	              date:'1/1/2013',
 	              time:'10.30am',
                   location:{
-                  			address:'Google Headquarters',
+                   			address:'Google Headquarters',
                   			city:'Mountain View',
                   			province:'CA' 
                             },
                   imageUrl:'/img/angularjs-logo.png',
                   sessions:[
                                {
-                               	name:'Directive Masterclass'
+                               	name:'Directive Masterclass',
+                               	creatorName:'Bob Smith',
+                               	duration:'1 hr',
+                               	level:'Advanced',
+                               	abstract:'In this session you\'ll learn about ins and outs of Directives'
+                                ,upVoteCount:3
                                },
 
                                {
-                               	name:'scopes'
+                               	name:'scopes',
+                               	creatorName:'Bob Smith',
+                               	duration:'2 hr',
+                               	level:'Advanced',
+                               	abstract:'In this session you\'ll learn about ins and outs of scopes'
+                               ,upVoteCount:2
                                },
 
                                {
-                               	name:'Well behaved controller'
+                               	name:'Well behaved controller',
+                               	creatorName:'Bob Smith',
+                               	duration:'3 hr',
+                               	level:'Advanced',
+                               	abstract:'In this session you\'ll learn about ins and outs of controller'
+                                ,upVoteCount:35
                                }
                   		   ]
 	             }
+    $scope.upVoteSession=function (session){
+    	session.upVoteCount++;
+    };
+    $scope.downVoteSession=function(session){
+    	session.upVoteCount--;
+    }; 	        
 }
 
 );
